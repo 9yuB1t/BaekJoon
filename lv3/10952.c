610d001428f016917cf836b2ce0b6a4e018e19de
9yuB1t <thsqjarb@gmail.com>
2025-11-09 22:01:38 +0900

    A+B-5 25-11-09

---
+++ lv3/10952.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    while(1){
+        int x,y;
+        scanf("%d %d",&x,&y);
+        if(x==0 && y ==0){
+            break;
+        }
+        printf("%d\n",x+y);
+    }
+    return 0;
+}
