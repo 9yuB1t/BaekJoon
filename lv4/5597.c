14f74336258ed41a3648a9c5bd8c2bc239d1d2a2
9yuB1t <thsqjarb@gmail.com>
2025-11-10 11:40:39 +0900

    과제안내신분? 25-11-10

---
+++ lv4/5597.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[30] = {0};
+    int num;
+    for(int i = 0; i < 28; i ++){
+        scanf("%d", &num);
+        arr[num-1] = 1;
+    }
+    for(int j = 0; j <30; j++){
+        if(arr[j]==0){
+            printf("%d\n",j);
+        }
+    }
+
+    return 0;
+}
