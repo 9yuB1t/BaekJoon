f66ffd00ca1013f851dbab9db60bcfba3d330b7c
9yuB1t <thsqjarb@gmail.com>
2025-11-11 14:46:12 +0900

    문자열반복 25-11-11

---
+++ lv5/2675.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char S[20];
+    int T, R;
+
+    scanf("%d",&T);
+
+    for(int i = 0; i < T; i++){
+        scanf("%d",&R);
+        scanf("%s", S);
+        for(int j = 0; j < (int)strlen(S); j++){
+            for(int k = 0; k < R; k++){
+                printf("%c", S[j]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
