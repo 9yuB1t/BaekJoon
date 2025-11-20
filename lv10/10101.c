b1252872a6ec9b68fc68eec9f1425ab57d0ed00c
9yuB1t <thsqjarb@gmail.com>
2025-11-20 14:38:04 +0900

    삼각형외우기 25-11-20

---
+++ lv10/10101.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a+b+c != 180){
+        printf("Error");
+        return 0;
+    }
+
+    if(a == b && b == c){
+        printf("Equilateral");
+    }else if( a== b || a == c || b == c ){
+         printf("Isosceles");
+    }else{
+        printf("Scalene");
+    }
+
+    return 0;
+}
