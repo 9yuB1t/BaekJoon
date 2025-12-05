1ac39a7df3c67939f1543493ded730de28a6d8ad
9yuB1t <thsqjarb@gmail.com>
2025-12-05 17:58:07 +0900

    소트인사이드 25-12-05

---
+++ lv13/1472.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int N;
+    int count[10] = {0,};
+    scanf("%d",&N);
+    while(N > 0){
+        count[N%10]++;
+        N /= 10;
+    }
+    for(int i = 9; i >= 0; i--){
+        for(int j = 0; j < count[i]; j++){
+            printf("%d",i);
+        }
+    }
+    return 0;
+}
