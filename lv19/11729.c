c42658e739b4bb125033d53d64bc8d63b6bf57c9
9yuB1t <thsqjarb@gmail.com>
2026-03-23 23:41:57 +0900

    하노이탑이동순서 26-03-23

---
+++ lv19/11729.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+void hanoi(int n, int from, int tmp, int to) {
+    if (n == 1) {
+        printf("%d %d\n", from, to);
+        return;
+    }
+
+    hanoi(n - 1, from, to, tmp);
+
+    printf("%d %d\n", from, to);
+
+    hanoi(n - 1, tmp, from, to);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int count = (1 << N) - 1;
+    printf("%d\n", count);
+
+    hanoi(N, 1, 2, 3);
+
+    return 0;
+}
